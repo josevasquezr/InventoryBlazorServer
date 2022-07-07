ce3975dc83cd1ab05e90a0784d6e9f451bf5ada9
jvasquez <javierv.ramos@gmail.com>
2022-07-06 22:36:49 -0600

    Implementando FuentValidation en CategoryEntity

--- Entities/CategoryEntity.cs
+++ Entities/CategoryEntity.cs
@@ -1,9 +1,20 @@
-﻿namespace Entities
+﻿using FluentValidation;
+
+namespace Entities
 {
     public class CategoryEntity
     {
         public Guid CategoryId { get; set; }
         public string CategoryName { get; set; }
         public ICollection<ProductEntity> Products { get; set; }
+    }
+
+    public class CategoryValidator : AbstractValidator<CategoryEntity>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(category => category.CategoryId).NotEmpty();
+            RuleFor(category => category.CategoryName).NotEmpty().Length(4, 100).Matches("([A-Za-z]+)\\s?([A-Za-z]+)");
+        }
     }
 }
